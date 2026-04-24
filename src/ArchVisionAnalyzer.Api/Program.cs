1c09bb1e76897406f303ef16018e1bb095eb76e3
Nursena Albayrak <nursenaalbayrak57@gmail.com>
2026-04-24 10:12:36 +0300

    Database bağlantısı düzeltildi

--- src/ArchVisionAnalyzer.Api/Program.cs
+++ src/ArchVisionAnalyzer.Api/Program.cs
@@ -6,7 +6,9 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration["DATABASE_URL"];
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString)); 
